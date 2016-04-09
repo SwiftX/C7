3cb6aca685c6a95c6582243a82734dac8a7167b0
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-08 22:32:08 -0300

    make now function and indentation for #if

--- Sources/Time.swift
+++ Sources/Time.swift
@@ -1,36 +1,34 @@
 #if os(Linux)
-import Glibc
+    import Glibc
 #else
-import Darwin.C
-var mtid = mach_timebase_info_data_t()
+    import Darwin.C
+    var mtid = mach_timebase_info_data_t()
 #endif
 
-public var now: Int {
+/// Milliseconds from January 1, 1970.
+public func now() -> Int {
     #if os(Linux)
-    var ts = timespec()
-    clock_gettime(CLOCK_MONOTONIC, &ts)
-    return Int(ts.tv_sec) * 1000 + Int(ts.tv_nsec) / 1000000
+        var ts = timespec()
+        clock_gettime(CLOCK_MONOTONIC, &ts)
+        return Int(ts.tv_sec) * 1000 + Int(ts.tv_nsec) / 1000000
     #else
-    if mtid.denom == 0 {
-        mach_timebase_info(&mtid)
-    }
-    let ticks = mach_absolute_time()
-    return Int(ticks * UInt64(mtid.numer) / UInt64(mtid.denom) / 1000000)
+        if mtid.denom == 0 {
+            mach_timebase_info(&mtid)
+        }
+        let ticks = mach_absolute_time()
+        return Int(ticks * UInt64(mtid.numer) / UInt64(mtid.denom) / 1000000)
     #endif
 }
 
 public extension Int {
     /// Interval of `self` from now.
-    var fromNow: Int {
-        return now + self
+    func fromNow() -> Int {
+        return now() + self
     }
 }
 
 public protocol TimeRepresentable {
     var millisecond: Int { get }
-    var second: Int { get }
-    var minute: Int { get }
-    var hour: Int { get }
 }
 
 extension TimeRepresentable {
