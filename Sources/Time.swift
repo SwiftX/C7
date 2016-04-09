43cd502da7bfe6ff9405323c05074b0d440ab4b9
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-09 16:52:31 -0300

    use 64 bits for timestamp

--- Sources/Time.swift
+++ Sources/Time.swift
@@ -6,7 +6,7 @@
 #endif
 
 /// Milliseconds from January 1, 1970.
-public func now() -> Int {
+public func now() -> Int64 {
     #if os(Linux)
         var ts = timespec()
         clock_gettime(CLOCK_MONOTONIC, &ts)
@@ -16,49 +16,53 @@ public func now() -> Int {
             mach_timebase_info(&mtid)
         }
         let ticks = mach_absolute_time()
-        return Int(ticks * UInt64(mtid.numer) / UInt64(mtid.denom) / 1000000)
+        return Int64(ticks * UInt64(mtid.numer) / UInt64(mtid.denom) / 1000000)
     #endif
 }
 
-public extension Int {
+extension Int64 {
     /// Interval of `self` from now.
-    func fromNow() -> Int {
-        return now() + self
+    public func fromNow() -> Int64 {
+        return now() + Int64(self.toIntMax())
     }
 }
 
-public protocol TimeRepresentable {
-    var millisecond: Int { get }
+public protocol TimeRepresentor {
+    var millisecond: Int64 { get }
 }
 
-extension TimeRepresentable {
-    public var milliseconds: Int {
+extension TimeRepresentor {
+    public var milliseconds: Int64 {
         return 1
     }
-    public var second: Int {
+    public var second: Int64 {
         return millisecond * 1000
     }
-    public var seconds: Int {
+    public var seconds: Int64 {
         return 1000
     }
-    public var minute: Int {
+    public var minute: Int64 {
         return millisecond * 60000
     }
-    public var minutes: Int {
+    public var minutes: Int64 {
         return 60000
     }
-    public var hour: Int {
+    public var hour: Int64 {
         return millisecond * 3600000
     }
-    public var hours: Int {
+    public var hours: Int64 {
         return 3600000
     }
 }
 
-extension Int: TimeRepresentable {
-    public var millisecond: Int {
-        return self
+extension Int: TimeRepresentor {
+    public var millisecond: Int64 {
+        return Int64(self.toIntMax())
     }
 }
 
-public let never = -1
+extension Int64 {
+    public static var never: Int64 {
+        return -1
+    }
+}
